4dcfd539064437efcf2235ff155022ef1338a46d
ochom <ochomrichard752@gmail.com>
2021-09-29 13:10:41 +0300

    --no-edit

--- middleware.go
+++ middleware.go
@@ -11,18 +11,24 @@ import (
 func AuthMidleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		reqToken := c.Request.Header.Get("Authorization")
-		splitToken := strings.Split(reqToken, " ")
-		reqToken = splitToken[1]
 
 		if reqToken == "" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "No Authorization header provided"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "No Authorization header provided"})
 			c.Abort()
 			return
 		}
 
+		splitToken := strings.Split(reqToken, " ")
+		if len(splitToken) != 2 {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid auth token provided"})
+			c.Abort()
+			return
+		}
+
+		reqToken = splitToken[1]
 		claims, err := ValidateToken(reqToken)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err})
 			c.Abort()
 			return
 		}
